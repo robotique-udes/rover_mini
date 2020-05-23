b40317b55dfb94d7d9b052775d828ed011fe5050
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2020-05-23 11:51:24 -0400

    corrected joy cmd bypassing the mux

--- osr/scripts/rover.py
+++ osr/scripts/rover.py
@@ -4,19 +4,10 @@ import rospy
 import message_filters
 from roboclaw_wrapper import MotorControllers
 from rover_udes.msg import Command
-from sensor_msgs.msg import Joy
 
 
 motorcontrollers0 = MotorControllers(0,128)
 motorcontrollers1 = MotorControllers(1,128)
-
-
-def joy_callback(data):
-	speedright = -data.axes[1]*100
-	speedleft = data.axes[4]*100
-	for i in range(2):
-		motorcontrollers0.sendSignedDutyAccel(i,speedleft)
-		motorcontrollers1.sendSignedDutyAccel(i,speedright)
 
 
 def cmd_mux_callback(data):
@@ -27,11 +18,9 @@ def cmd_mux_callback(data):
 		motorcontrollers1.sendSignedDutyAccel(i, speedright)
 
 
-
 if __name__ == '__main__':
 	rospy.init_node('rover')
 	rospy.loginfo("Starting the rover node")
-	joy_sub = rospy.Subscriber("/joy", Joy, joy_callback)
 	cmd_mux_sub = rospy.Subscriber("/mux_cmd", Command, cmd_mux_callback)
 	rate = rospy.Rate(10)
 	rate.sleep()
